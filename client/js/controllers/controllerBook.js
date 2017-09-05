e326450a4ad6fa45506351da869f9b8f5991a4ab
Bruno Auzier <auzierk13@gmail.com>
2017-09-05 16:56:25 -0400

    Adicionando /lista-book os métodos Buscar,ordenar e paginar

--- client/js/controllers/controllerBook.js
+++ client/js/controllers/controllerBook.js
@@ -2,7 +2,7 @@
 angular
 	.module('app')
 	.controller('BookListaController',['$scope','$state','Book', function($scope, $state, Book){
-		$scope.books ={};
+		$scope.books =[];
 		function listaBook(){
 			Book.find().$promise.then(function(res,err){
 				$scope.books= res;
@@ -10,4 +10,28 @@ angular
 		}
 
 		listaBook();
+		//###Sort books 
+		$scope.sortField = "id";
+ 	 	$scope.reverse=false;
+ 	 	$scope.isIcones=[	{nome: "id", selectIcone: false, img:"glyphicon glyphicon-chevron-up"},
+							{nome: "firstname", selectIcone: false, img:"glyphicon glyphicon-chevron-up"},
+							{nome: "lastname", selectIcone: false, img:"glyphicon glyphicon-chevron-up"}
+					 	];
+	function chooseIcone (sortBook) {
+		 $scope.isIcones.forEach( function(icone) {
+		 	if(icone.nome === sortBook){
+				icone.selectIcone=!icone.selectIcone; //Inverte o icone
+		 	}else{
+		 		icone.selectIcone=false;
+		 	}
+				!icone.selectIcone? icone.img="glyphicon glyphicon-chevron-up":icone.img="glyphicon glyphicon-chevron-down";
+		 });
+	}
+
+		$scope.sort = function(sortBook){
+			console.log(sortBook);
+			$scope.sortField=sortBook;
+			$scope.reverse = !$scope.reverse;
+			chooseIcone(sortBook);
+		}
 	}]);
