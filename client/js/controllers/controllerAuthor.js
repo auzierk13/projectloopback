3fe55bf8c5fb6e289b5ec629d5d654809e1371ce
Bruno Auzier <auzierk13@gmail.com>
2017-09-05 08:06:07 -0400

    Adicionando método editar author

--- client/js/controllers/controllerAuthor.js
+++ client/js/controllers/controllerAuthor.js
@@ -13,11 +13,30 @@ angular
 
 		$scope.recoverAuthor = function(author){
 			    $scope.dataAuthor = author;
+				console.log($scope.dataAuthor);
 			}
 
-		$scope.editaAuthor = function(author){
-			console.log("Editar");
-			console.log(author);
+		$scope.editaAuthor = function(editAuthor){
+
+			if(!editAuthor){
+				console.log("Erro ao Editar");
+			}else{
+				console.log("Editar");
+				if(editAuthor.firstname){
+					$scope.dataAuthor.firstname = editAuthor.firstname; 
+					console.log('Não possui firstname');
+				}else if(editAuthor.lastname){
+					$scope.dataAuthor.lastname = editAuthor.lastname; 
+					// editAuthor.lastname = $scope.dataAuthor.lastname;
+					console.log('Não possui lastname');
+				}
+				// editAuthor.id = $scope.dataAuthor.id; //recebe id selecionado
+				delete $scope.editAuthor;
+				console.log($scope.dataAuthor);
+				Author.replaceById($scope.dataAuthor.id,$scope.dataAuthor, function(err, res) {
+					listaAuthor();
+				});
+			}
 		}
 
 		$scope.removeAuthor = function(author){
@@ -26,6 +45,7 @@ angular
 		}
 
 		listaAuthor();
+
 	}])
 
 	.controller('AuthorAdicionaController',['$scope','$state','Author', function($scope, $state, Author){
